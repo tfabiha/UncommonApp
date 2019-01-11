010ab3a80a58cbaf5b1f2dc08aa6499dad86cb40
Michelle Tang <mtang1@stuy.edu>
2019-01-11 12:34:28 -0500

    database

--- util/db_builder.py
+++ util/db_builder.py
@@ -1,24 +1,34 @@
 import sqlite3 #imports sqlite
 
 def users(): #creates the users db
-    DB_FILE="./data/UncommonApp.db" 
+    DB_FILE="../data/userInfo.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitates db operations
-    command = "CREATE TABLE users(username TEXT, password TEXT, score INTEGER)"
+    command = "CREATE TABLE users(username TEXT, password TEXT, moves INTEGER, likedPuzzles TEXT)"
     c.execute(command)
 
-def questions(): #creates the questions db
-    DB_FILE="./data/AllDogsGoToHeaven.db" 
+def questions(): #creates the puzzles db
+    DB_FILE="../data/puzzles.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitates db operations
-    command = "CREATE TABLE questions(username TEXT, question TEXT)"
+    command = "CREATE TABLE puzzles(puzzleID INTEGER, puzzle_content TEXT, averageMoves INT)"
+    c.execute(command)
+
+def logs(): #creates the logs db
+    DB_FILE="../data/logs.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor() #facilitates db operations
+    command = "CREATE TABLE logs(username TEXT, moves INTEGER, puzzleID INTEGER)"
     c.execute(command)
 
 def main(): #calls all of the functions to build the databases
     try:
-        
         users()
         questions()
+        logs()
+
     except:
         pass
-    
+
+main()
+print("finished creating all of the databases")
