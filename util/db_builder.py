7bfc8d13cbc4d6bcd7758b978205e21c1dbb9afe
Michelle Tang <mtang1@stuy.edu>
2019-01-13 13:27:55 -0500

    separate high scores

--- util/db_builder.py
+++ util/db_builder.py
@@ -1,21 +1,21 @@
 import sqlite3 #imports sqlite
 
 def users(): #creates the users db
-    DB_FILE="./data/userInfo.db"
+    DB_FILE="../data/userInfo.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitates db operations
     command = "CREATE TABLE users(username TEXT, password TEXT, moves INTEGER, likedPuzzles TEXT)"
     c.execute(command)
 
-def questions(): #creates the puzzles db
-    DB_FILE="./data/puzzles.db"
+def puzzles(): #creates the puzzles db
+    DB_FILE="../data/puzzles.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitates db operations
     command = "CREATE TABLE puzzles(puzzleID INTEGER, puzzle_content TEXT, averageMoves INT)"
     c.execute(command)
 
 def logs(): #creates the logs db
-    DB_FILE="./data/logs.db"
+    DB_FILE="../data/logs.db"
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor() #facilitates db operations
     command = "CREATE TABLE logs(username TEXT, moves INTEGER, puzzleID INTEGER)"
@@ -24,9 +24,8 @@ def logs(): #creates the logs db
 def main(): #calls all of the functions to build the databases
     try:
         users()
-        questions()
+        puzzles()
         logs()
-
     except:
         pass
 
